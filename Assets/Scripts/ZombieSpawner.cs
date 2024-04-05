c7563a7eab72d754e127b08b6ebd6355ef480c7f
Jae <55465890+kimj80@users.noreply.github.com>
2024-04-04 20:23:24 -0600

    Zombie spawn fixed
    Zombie spawn fixed

--- Assets/Scripts/ZombieSpawner.cs
+++ Assets/Scripts/ZombieSpawner.cs
@@ -22,44 +22,62 @@ public class Spawner : MonoBehaviour
     [SerializeField]
     private float spawnZEnd = 0.0f;
 
-    private int[] table = {50, 25, 20, 5}; //spawn chances of zombies
+    //private int[] table = {50, 25, 20, 5}; //spawn chances of zombies
 
-    private int total;
+    //private int total;
 
+    [SerializeField]
+    private int spawnerLimit = 10;
 
+    private int spawns = 0;
+
+    //private int randomNum;
+    private int randomZSpawn;
 
     // Start is called before the first frame update
     void Start()
     {
-        foreach(var item in table)
-        {
-            total += item;
-        }
+        //foreach (var item in table)
+        //{
+        //    total += item;
+        //}
         StartCoroutine(spawnEnemy(zombieInterval));
-        
     }
 
     private IEnumerator spawnEnemy(float interval)
     {
-        yield return new WaitForSeconds(interval);
-        GameObject newZombie = getZombie();
-        StartCoroutine(spawnEnemy(interval));
+        while (spawns < spawnerLimit)
+        {
+            yield return new WaitForSeconds(interval);
+            getZombie();
+            spawns++;
+            //GameObject newZombie = getZombie();
+            //StartCoroutine(spawnEnemy(interval));
+        }
     }
 
-    private GameObject getZombie() {
-        int randomNum = Random.Range(0, total);
-        GameObject newZombie = zombies[0];
-        for (int i = 0; i < table.Length; i++) {
-            if (randomNum <= table[i]) {
-                newZombie = Instantiate(zombies[i], new Vector3(Random.Range(spawnXStart, spawnXEnd), 6.5f, Random.Range(spawnZStart, spawnZEnd)), Quaternion.identity);
-                return newZombie;
-            }
-            else 
-            {
-                randomNum -= table[i];
-            }
-        }
-        return newZombie;
+    private void getZombie() {
+        //randomNum = Random.Range(0, total);
+        randomZSpawn = Random.Range(0, zombies.Count);
+
+        Instantiate(zombies[randomZSpawn], new Vector3(Random.Range(spawnXStart, spawnXEnd), 6.5f, Random.Range(spawnZStart, spawnZEnd)), Quaternion.identity);
+        //GameObject newZombie = zombies[0];
+        //StartCoroutine(spawnEnemy(zombieInterval));
+        
+
+        //for (int i = 0; i < table.Length; i++)
+        //{
+        //    if (randomNum <= table[i])
+        //    {
+        //        //newZombie = Instantiate(zombies[i], new Vector3(Random.Range(spawnXStart, spawnXEnd), 6.5f, Random.Range(spawnZStart, spawnZEnd)), Quaternion.identity);
+        //        Instantiate(zombies[randomZSpawn], new Vector3(Random.Range(spawnXStart, spawnXEnd), 6.5f, Random.Range(spawnZStart, spawnZEnd)), Quaternion.identity);
+        //    }
+        //    else
+        //    {
+        //        randomNum -= table[i];
+        //    }
+        //}
+        return;
     }
 
 }
